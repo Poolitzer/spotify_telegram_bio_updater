a1c90c3097d03b821df35ea22144873ab8299d32
Poolitzer <25934244+Poolitzer@users.noreply.github.com>
2022-01-09 09:18:21 +0100

    Feat: Running black

--- bot.py
+++ bot.py
@@ -1,5 +1,14 @@
 import asyncio
-from constants import API_HASH, API_ID, CLIENT_ID, CLIENT_SECRET, LOG, SHUTDOWN_COMMAND, BIOS, LIMIT
+from constants import (
+    API_HASH,
+    API_ID,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    LOG,
+    SHUTDOWN_COMMAND,
+    BIOS,
+    LIMIT,
+)
 import json
 import logging
 from aiohttp import ClientSession
@@ -7,25 +16,35 @@ from telethon import TelegramClient, events
 from telethon.errors import FloodWaitError, AboutTooLongError
 from telethon.tl.functions.account import UpdateProfileRequest
 from telethon.tl.functions.users import GetFullUserRequest
+
 device_model = "spotify_bot"
 version = "1.5"
 system_version, app_version = version, version
-client = TelegramClient('spotify', API_ID, API_HASH, device_model=device_model,
-                        system_version=system_version, app_version=app_version)
-logging.basicConfig(level=logging.ERROR, filename='log.log',
-                    format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s')
+client = TelegramClient(
+    "spotify",
+    API_ID,
+    API_HASH,
+    device_model=device_model,
+    system_version=system_version,
+    app_version=app_version,
+)
+logging.basicConfig(
+    level=logging.ERROR,
+    filename="log.log",
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+)
 logger = logging.getLogger(__name__)
 
 
 def ms_converter(millis):
     millis = int(millis)
-    seconds = (millis/1000) % 60
+    seconds = (millis / 1000) % 60
     seconds = int(seconds)
-    if str(seconds) == '0':
-        seconds = '00'
+    if str(seconds) == "0":
+        seconds = "00"
     if len(str(seconds)) == 1:
-        seconds = '0' + str(seconds)
-    minutes = (millis/(1000*60)) % 60
+        seconds = "0" + str(seconds)
+    minutes = (millis / (1000 * 60)) % 60
     minutes = int(minutes)
     return str(minutes) + ":" + str(seconds)
 
@@ -66,7 +85,7 @@ class Database:
         return self.db[which + "_spam"]
 
     def save(self):
-        with open('./database.json', 'w') as outfile:
+        with open("./database.json", "w") as outfile:
             json.dump(self.db, outfile, indent=4, sort_keys=True)
 
 
@@ -105,9 +124,10 @@ async def work(session: ClientSession):
         # SPOTIFY
         skip = False
         to_insert = {}
-        oauth = {
-            "Authorization": "Bearer " + database.return_token()}
-        async with session.get('https://api.spotify.com/v1/me/player/currently-playing', headers=oauth) as response:
+        oauth = {"Authorization": "Bearer " + database.return_token()}
+        async with session.get(
+            "https://api.spotify.com/v1/me/player/currently-playing", headers=oauth
+        ) as response:
             status_code = response.status
             # 200 means user plays smth
             if status_code == 200:
@@ -115,40 +135,56 @@ async def work(session: ClientSession):
                 if received["currently_playing_type"] == "track":
                     to_insert["title"] = received["item"]["name"]
                     to_insert["progress"] = ms_converter(received["progress_ms"])
-                    to_insert["interpret"] = received['item']["artists"][0]["name"]
-                    to_insert["duration"] = ms_converter(received["item"]["duration_ms"])
+                    to_insert["interpret"] = received["item"]["artists"][0]["name"]
+                    to_insert["duration"] = ms_converter(
+                        received["item"]["duration_ms"]
+                    )
                     if save_spam("spotify", False):
-                        stringy = "**[INFO]**\n\nEverything returned back to normal, the previous spotify issue has been " \
-                                  "resolved."
+                        stringy = (
+                            "**[INFO]**\n\nEverything returned back to normal, the previous spotify issue has been "
+                            "resolved."
+                        )
                         await client.send_message(LOG, stringy)
                 else:
                     if save_spam("spotify", True):
                         # currently item is not passed when the user plays a podcast
-                        string = f"**[INFO]**\n\nThe playback {received['currently_playing_type']} didn't gave me any " \
+                        string = (
+                            f"**[INFO]**\n\nThe playback {received['currently_playing_type']} didn't gave me any "
                             f"additional information, so I skipped updating the bio."
+                        )
                         await client.send_message(LOG, string)
             # 429 means flood limit, we need to wait
             elif status_code == 429:
-                to_wait = response.headers['Retry-After']
+                to_wait = response.headers["Retry-After"]
                 logger.error(f"Spotify, have to wait for {str(to_wait)}")
-                await client.send_message(LOG, f'**[WARNING]**\n\nI caught a spotify api limit. I shall sleep for '
-                                               f'{str(to_wait)} seconds until I refresh again')
+                await client.send_message(
+                    LOG,
+                    f"**[WARNING]**\n\nI caught a spotify api limit. I shall sleep for "
+                    f"{str(to_wait)} seconds until I refresh again",
+                )
                 skip = True
                 await asyncio.sleep(int(to_wait))
             # 204 means user plays nothing, since to_insert is false, we dont need to change anything
             elif status_code == 204:
                 if save_spam("spotify", False):
-                    stringy = "**[INFO]**\n\nEverything returned back to normal, the previous spotify issue has been " \
-                              "resolved."
+                    stringy = (
+                        "**[INFO]**\n\nEverything returned back to normal, the previous spotify issue has been "
+                        "resolved."
+                    )
                     await client.send_message(LOG, stringy)
                 pass
             # 401 means our access token is expired, so we need to refresh it
             elif status_code == 401:
-                data = {"client_id": CLIENT_ID, "client_secret": CLIENT_SECRET,
-                        "grant_type": "refresh_token",
-                        "refresh_token": database.return_refresh()}
+                data = {
+                    "client_id": CLIENT_ID,
+                    "client_secret": CLIENT_SECRET,
+                    "grant_type": "refresh_token",
+                    "refresh_token": database.return_refresh(),
+                }
 
-                async with session.post("https://accounts.spotify.com/api/token", data=data) as post_response:
+                async with session.post(
+                    "https://accounts.spotify.com/api/token", data=data
+                ) as post_response:
                     received = await post_response.json()
                     # if a new refresh is token as well, we save it here
                     try:
@@ -158,20 +194,24 @@ async def work(session: ClientSession):
                     database.save_token(received["access_token"])
                     # since we didnt actually update our status yet, lets do this without the 30 seconds wait
                     skip = True
-            # 502 means bad gateway, its an issue on spotify site which we can do nothing about. 30 seconds wait shouldn't
-            # put too much pressure on the spotify server, so we are just going to notify the user once
+            # 502 means bad gateway, its an issue on spotify site which we can do nothing about. 30 seconds wait
+            # shouldn't put too much pressure on the spotify server, so we are just going to notify the user once
             elif status_code == 502:
                 if save_spam("spotify", True):
-                    string = f"**[WARNING]**\n\nSpotify returned a Bad gateway, which means they have a problem on their " \
+                    string = (
+                        f"**[WARNING]**\n\nSpotify returned a Bad gateway, which means they have a problem on their "
                         f"servers. The bot will continue to run but may not update the bio for a short time."
+                    )
                     await client.send_message(LOG, string)
             # 503 means service unavailable, its an issue on spotify site which we can do nothing about. 30 seconds wait
             # shouldn't put too much pressure on the spotify server, so we are just going to notify the user once
             elif status_code == 503:
                 if save_spam("spotify", True):
-                    string = f"**[WARNING]**\n\nSpotify said that the service is unavailable, which means they have a " \
-                             f"problem on their servers. The bot will continue to run but may not update the bio for a " \
-                             f"short time."
+                    string = (
+                        f"**[WARNING]**\n\nSpotify said that the service is unavailable, which means they have a "
+                        f"problem on their servers. The bot will continue to run but may not update the bio for a "
+                        f"short time."
+                    )
                     await client.send_message(LOG, string)
             # 404 is a spotify error which isn't supposed to happen (since our URL is correct). Track the issue here:
             # https://github.com/spotify/web-api/issues/1280
@@ -181,15 +221,23 @@ async def work(session: ClientSession):
                     await client.send_message(LOG, string)
             # catch anything else
             else:
-                await client.send_message(LOG, '**[ERROR]**\n\nOK, so something went reeeally wrong with spotify. The bot '
-                                               'was stopped.\nStatus code: ' + str(status_code) + '\n\nText: ' + await response.text())
-                logger.error(f"Spotify, error {str(status_code)}, text: {await response.text()}")
+                await client.send_message(
+                    LOG,
+                    "**[ERROR]**\n\nOK, so something went reeeally wrong with spotify. The bot "
+                    "was stopped.\nStatus code: "
+                    + str(status_code)
+                    + "\n\nText: "
+                    + await response.text(),
+                )
+                logger.error(
+                    f"Spotify, error {str(status_code)}, text: {await response.text()}"
+                )
                 # stop the whole program since I dont know what happens here and this is the safest thing we can do
                 asyncio.get_running_loop().stop()
             # TELEGRAM
             try:
                 # full needed, since we dont get a bio with the normal request
-                full = await client(GetFullUserRequest('me'))
+                full = await client(GetFullUserRequest("me"))
                 bio = full.about
                 # to_insert means we have a successful playback
                 if to_insert:
@@ -201,7 +249,12 @@ async def work(session: ClientSession):
                     # we need this variable to see if actually one of the bios is below the character limit
                     new_bio = ""
                     for bio in BIOS:
-                        temp = bio.format(title=title, interpret=interpret, progress=progress, duration=duration)
+                        temp = bio.format(
+                            title=title,
+                            interpret=interpret,
+                            progress=progress,
+                            duration=duration,
+                        )
                         # we try to not ignore for telegrams character limit here
                         if len(temp) < LIMIT:
                             # this is short enough, so we put it in the variable and break our for loop
@@ -220,28 +273,36 @@ async def work(session: ClientSession):
                             try:
                                 await client(UpdateProfileRequest(about=new_bio))
                                 if save_spam("telegram", False):
-                                    stringy = "**[INFO]**\n\nEverything returned back to normal, the previous telegram " \
-                                              "issue has been resolved."
+                                    stringy = (
+                                        "**[INFO]**\n\nEverything returned back to normal, the previous telegram "
+                                        "issue has been resolved."
+                                    )
                                     await client.send_message(LOG, stringy)
                             # this can happen if our LIMIT check failed because telegram counts emojis twice and python
                             # doesnt. Refer to the constants file to learn more about this
                             except AboutTooLongError:
                                 if save_spam("telegram", True):
-                                    stringy = f'**[WARNING]**\n\nThe biography I tried to insert was too long. In order ' \
-                                        f'to not let that happen again in the future, please read the part about OFFSET ' \
-                                        f'in the constants. Anyway, here is the bio I tried to insert:\n\n{new_bio}'
+                                    stringy = (
+                                        f"**[WARNING]**\n\nThe biography I tried to insert was too long. In order "
+                                        f"to not let that happen again in the future, please read the part about OFFSET "
+                                        f"in the constants. Anyway, here is the bio I tried to insert:\n\n{new_bio}"
+                                    )
                                     await client.send_message(LOG, stringy)
                     # if we dont have a bio, everything was too long, so we tell the user that
                     if not new_bio:
                         if save_spam("telegram", True):
-                            to_send = f"**[INFO]**\n\nThe current track exceeded the character limit, so the bio wasn't " \
+                            to_send = (
+                                f"**[INFO]**\n\nThe current track exceeded the character limit, so the bio wasn't "
                                 f"updated.\n\n Track: {title}\nInterpret: {interpret}"
+                            )
                             await client.send_message(LOG, to_send)
                 # not to_insert means no playback
                 else:
                     if save_spam("telegram", False):
-                        stringy = "**[INFO]**\n\nEverything returned back to normal, the previous telegram issue has " \
-                                  "been resolved."
+                        stringy = (
+                            "**[INFO]**\n\nEverything returned back to normal, the previous telegram issue has "
+                            "been resolved."
+                        )
                         await client.send_message(LOG, stringy)
                     old_bio = database.return_bio()
                     # this means the bio is blank, so we save that as the new one
@@ -259,8 +320,11 @@ async def work(session: ClientSession):
             except FloodWaitError as e:
                 to_wait = e.seconds
                 logger.error(f"to wait for {str(to_wait)}")
-                await client.send_message(LOG, f'**[WARNING]**\n\nI caught a telegram api limit. I shall sleep '
-                                               f'{str(to_wait)} seconds until I refresh again')
+                await client.send_message(
+                    LOG,
+                    f"**[WARNING]**\n\nI caught a telegram api limit. I shall sleep "
+                    f"{str(to_wait)} seconds until I refresh again",
+                )
                 skip = True
                 await asyncio.sleep(int(to_wait))
             # skip means a flood error stopped the whole program, no need to wait another 30 seconds after that
@@ -292,6 +356,7 @@ async def main():
         loop.create_task(startup())
         await client.run_until_disconnected()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
     logger.error("Shut down complete")
