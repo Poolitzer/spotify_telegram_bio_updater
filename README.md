13a486294f5faf3e57e04b94301274f2c0bf0749
Poolitzer <25934244+Poolitzer@users.noreply.github.com>
2022-01-10 16:57:26 +0100

    (1.6) Feat: Revamp of setup process

--- README.md
+++ README.md
@@ -3,15 +3,19 @@ This userbot updates the biography of a telegram user according to their current
 
 ## Setup
 
-If you follow this steps, you can use this project easily for yourself.
+This script will do an interactive setup the first time you run bot.py. If you want to do it by yourself, you can follow the steps below, but that isn't necessary anymore.
 
-0. Ensure you have python 3.6+ installed and then install telethon and request via pip: `pip install telethon requests`
-1. Get your spotify client_id and your client_secret from https://developer.spotify.com/dashboard/. If you need more help, have a look [here](https://developer.spotify.com/documentation/general/guides/app-settings/#register-your-app).
+Just remember to run `pip install -r requirements.txt`.
+
+Depreciated manual steps:
+
+0. Ensure you have python 3.6+ installed and then run `pip install -r requirements.txt`.
+1. Get your spotify client_id and your client_secret from https://developer.spotify.com/dashboard/. If you need more help, have a look [here](https://developer.spotify.com/documentation/general/guides/app-settings/#register-your-app). Set a Rediret URI to http://localhost:1234/callback
 2. Get your telegram app api_id and api_hash from https://my.telegram.org/. If you need more help, have a look [here](https://telethon.readthedocs.io/en/latest/extra/basic/creating-a-client.html#creating-a-client).
-3. Open the following link (change CLIENT_ID to you client_id): https://accounts.spotify.com/authorize?client_id=CLIENT_ID&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-playback-state%20user-read-currently-playing
-4. After you grant permission, you get redirected to https://example.com/callback?code=_. Copy everything after the code, this is you initial token.
-5. Paste all these values in their respective variables at [constants.py](/constants.py). While you are at it, you can also paste an initial biography there. Just take your current one. This is highly recommended. If you don't do this **and** have a currently playing track, the bot has at its first start no idea what your original biography is. Just do it, please.
-6. If you want to have a log channel or group or so, paste its invite link or id in the LOG variable. If you leave it at "me", you will see those in your saved chat. Only if errors occur ofc ;)
+3. If you want to have a log channel or group or so, paste its invite link or id in the LOG variable. If you leave it at "me", you will see those in your saved chat. Only if errors occur ofc ;)
+4. Paste all these values in their respective variables at [constants.py](/constants.py). While you are at it, you can also paste an initial biography there. Just take your current one. This is highly recommended. If you don't do this **and** have a currently playing track, the bot has at its first start no idea what your original biography is. Just do it, please.
+5. You are going to generate a token here which is only valid for a short amount of time, so be sure to run generate.py quickly after this. Open the following link (change CLIENT_ID to you client_id): https://accounts.spotify.com/authorize?client_id=CLIENT_ID&response_type=code&redirect_uri=http://localhost:1234/callback&scope=user-read-playback-state%20user-read-currently-playing
+6. After you grant permission, you get redirected to https://example.com/callback?code=_. Copy everything after the code, this is you initial token.
 7. Now you can run [generate.py](/generate.py). This will generate a json file named database.
 8. You are almost done. If you now run [bot.py](/bot.py), all you need to do is log into your telegram account. Follow the instructions on screen.
 9. Now you are really done.
--- bot.py
+++ bot.py
@@ -1,39 +1,73 @@
 import asyncio
-from constants import (
-    API_HASH,
-    API_ID,
-    CLIENT_ID,
-    CLIENT_SECRET,
-    LOG,
-    SHUTDOWN_COMMAND,
-    BIOS,
-    LIMIT,
-)
+import os.path
 import json
 import logging
+
 from aiohttp import ClientSession
 from telethon import TelegramClient, events
 from telethon.errors import FloodWaitError, AboutTooLongError
 from telethon.tl.functions.account import UpdateProfileRequest
 from telethon.tl.functions.users import GetFullUserRequest
 
+import constants
+from setup import setup, intro_screen, update_constants
+
+# values supplied to telegram
 device_model = "spotify_bot"
-version = "1.5"
+version = "1.6"
 system_version, app_version = version, version
+
+# if the intro screen has already been printed
+intro_printed = False
+
+# this check allows the user to not having to enter any constants into constants.py prior to using this script
+if constants.API_ID == 0:
+    print(intro_screen)
+    intro_printed = True
+    constants.API_ID = int(
+        input(
+            "Hello there, thanks for using this little project. Please enter your Telegram App api_id. You can get"
+            " it at https://my.telegran.org: "
+        )
+    )
+    lines = [2]
+    replacements = [f"API_ID = {constants.API_ID}"]
+    # a bit better output strings, though its a bit weird if someone only enters one of the values
+    if not constants.API_HASH:
+        constants.API_HASH = input("\nGreat, now you need to copy paste the api_hash: ")
+        lines.append(3)
+        replacements.append(f'API_HASH = "{constants.API_HASH}"')
+    update_constants(lines, replacements)
+# again, weird if this is triggered, but who am I to judge
+if not constants.API_HASH:
+    print(intro_screen)
+    intro_printed = True
+    constants.API_HASH = input(
+        "Hello there, thanks for using this little project. Please enter your Telegram App api_hash. You can get"
+        " it at https://my.telegran.org: "
+    )
+    lines = [3]
+    replacements = [f'API_HASH = "{constants.API_HASH}"']
+    update_constants(lines, replacements)
+
 client = TelegramClient(
     "spotify",
-    API_ID,
-    API_HASH,
+    constants.API_ID,
+    constants.API_HASH,
     device_model=device_model,
     system_version=system_version,
     app_version=app_version,
 )
+
 logging.basicConfig(
     level=logging.ERROR,
     filename="log.log",
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
 )
 logger = logging.getLogger(__name__)
+
+aiohttp_server_logger = logging.getLogger("aiohttp.server")
+aiohttp_server_logger.disabled = True
 
 
 def ms_converter(millis):
@@ -54,8 +88,8 @@ class Database:
         try:
             self.db = json.load(open("./database.json"))
         except FileNotFoundError:
-            print("You need to run generate.py first, please read the Readme.")
-            asyncio.get_running_loop().stop()
+            # this will be the case when someone hasn't run the setup yet, it will be recalled when the file exists
+            self.db = {}
 
     def save_token(self, token):
         self.db["access_token"] = token
@@ -91,11 +125,10 @@ class Database:
 
 database = Database()
 
+
 # to stop unwanted spam, we sent these type of message only once. So we have a variable in our database which we check
 # for in return_info. When we send a message, we set this variable to true. After a successful update
 # (or a closing of spotify), we reset that variable to false.
-
-
 def save_spam(which, what):
     # see below why
 
@@ -144,7 +177,7 @@ async def work(session: ClientSession):
                             "**[INFO]**\n\nEverything returned back to normal, the previous spotify issue has been "
                             "resolved."
                         )
-                        await client.send_message(LOG, stringy)
+                        await client.send_message(constants.LOG, stringy)
                 else:
                     if save_spam("spotify", True):
                         # currently item is not passed when the user plays a podcast
@@ -152,13 +185,13 @@ async def work(session: ClientSession):
                             f"**[INFO]**\n\nThe playback {received['currently_playing_type']} didn't gave me any "
                             f"additional information, so I skipped updating the bio."
                         )
-                        await client.send_message(LOG, string)
+                        await client.send_message(constants.LOG, string)
             # 429 means flood limit, we need to wait
             elif status_code == 429:
                 to_wait = response.headers["Retry-After"]
                 logger.error(f"Spotify, have to wait for {str(to_wait)}")
                 await client.send_message(
-                    LOG,
+                    constants.LOG,
                     f"**[WARNING]**\n\nI caught a spotify api limit. I shall sleep for "
                     f"{str(to_wait)} seconds until I refresh again",
                 )
@@ -171,13 +204,13 @@ async def work(session: ClientSession):
                         "**[INFO]**\n\nEverything returned back to normal, the previous spotify issue has been "
                         "resolved."
                     )
-                    await client.send_message(LOG, stringy)
+                    await client.send_message(constants.LOG, stringy)
                 pass
             # 401 means our access token is expired, so we need to refresh it
             elif status_code == 401:
                 data = {
-                    "client_id": CLIENT_ID,
-                    "client_secret": CLIENT_SECRET,
+                    "client_id": constants.CLIENT_ID,
+                    "client_secret": constants.CLIENT_SECRET,
                     "grant_type": "refresh_token",
                     "refresh_token": database.return_refresh(),
                 }
@@ -202,7 +235,7 @@ async def work(session: ClientSession):
                         f"**[WARNING]**\n\nSpotify returned a Bad gateway, which means they have a problem on their "
                         f"servers. The bot will continue to run but may not update the bio for a short time."
                     )
-                    await client.send_message(LOG, string)
+                    await client.send_message(constants.LOG, string)
             # 503 means service unavailable, its an issue on spotify site which we can do nothing about. 30 seconds wait
             # shouldn't put too much pressure on the spotify server, so we are just going to notify the user once
             elif status_code == 503:
@@ -212,17 +245,17 @@ async def work(session: ClientSession):
                         f"problem on their servers. The bot will continue to run but may not update the bio for a "
                         f"short time."
                     )
-                    await client.send_message(LOG, string)
+                    await client.send_message(constants.LOG, string)
             # 404 is a spotify error which isn't supposed to happen (since our URL is correct). Track the issue here:
             # https://github.com/spotify/web-api/issues/1280
             elif status_code == 404:
                 if save_spam("spotify", True):
                     string = f"**[INFO]**\n\nSpotify returned a 404 error, which is a bug on their side."
-                    await client.send_message(LOG, string)
+                    await client.send_message(constants.LOG, string)
             # catch anything else
             else:
                 await client.send_message(
-                    LOG,
+                    constants.LOG,
                     "**[ERROR]**\n\nOK, so something went reeeally wrong with spotify. The bot "
                     "was stopped.\nStatus code: "
                     + str(status_code)
@@ -248,7 +281,7 @@ async def work(session: ClientSession):
                     duration = to_insert["duration"]
                     # we need this variable to see if actually one of the bios is below the character limit
                     new_bio = ""
-                    for bio in BIOS:
+                    for bio in constants.BIOS:
                         temp = bio.format(
                             title=title,
                             interpret=interpret,
@@ -256,7 +289,7 @@ async def work(session: ClientSession):
                             duration=duration,
                         )
                         # we try to not ignore for telegrams character limit here
-                        if len(temp) < LIMIT:
+                        if len(temp) < constants.LIMIT:
                             # this is short enough, so we put it in the variable and break our for loop
                             new_bio = temp
                             break
@@ -277,17 +310,17 @@ async def work(session: ClientSession):
                                         "**[INFO]**\n\nEverything returned back to normal, the previous telegram "
                                         "issue has been resolved."
                                     )
-                                    await client.send_message(LOG, stringy)
+                                    await client.send_message(constants.LOG, stringy)
                             # this can happen if our LIMIT check failed because telegram counts emojis twice and python
                             # doesnt. Refer to the constants file to learn more about this
                             except AboutTooLongError:
                                 if save_spam("telegram", True):
                                     stringy = (
                                         f"**[WARNING]**\n\nThe biography I tried to insert was too long. In order "
-                                        f"to not let that happen again in the future, please read the part about OFFSET "
-                                        f"in the constants. Anyway, here is the bio I tried to insert:\n\n{new_bio}"
+                                        f"to not let that happen again in the future, please read the part about OFFSET"
+                                        f" in the constants. Anyway, here is the bio I tried to insert:\n\n{new_bio}"
                                     )
-                                    await client.send_message(LOG, stringy)
+                                    await client.send_message(constants.LOG, stringy)
                     # if we dont have a bio, everything was too long, so we tell the user that
                     if not new_bio:
                         if save_spam("telegram", True):
@@ -295,7 +328,7 @@ async def work(session: ClientSession):
                                 f"**[INFO]**\n\nThe current track exceeded the character limit, so the bio wasn't "
                                 f"updated.\n\n Track: {title}\nInterpret: {interpret}"
                             )
-                            await client.send_message(LOG, to_send)
+                            await client.send_message(constants.LOG, to_send)
                 # not to_insert means no playback
                 else:
                     if save_spam("telegram", False):
@@ -303,7 +336,7 @@ async def work(session: ClientSession):
                             "**[INFO]**\n\nEverything returned back to normal, the previous telegram issue has "
                             "been resolved."
                         )
-                        await client.send_message(LOG, stringy)
+                        await client.send_message(constants.LOG, stringy)
                     old_bio = database.return_bio()
                     # this means the bio is blank, so we save that as the new one
                     if not bio:
@@ -321,7 +354,7 @@ async def work(session: ClientSession):
                 to_wait = e.seconds
                 logger.error(f"to wait for {str(to_wait)}")
                 await client.send_message(
-                    LOG,
+                    constants.LOG,
                     f"**[WARNING]**\n\nI caught a telegram api limit. I shall sleep "
                     f"{str(to_wait)} seconds until I refresh again",
                 )
@@ -334,14 +367,18 @@ async def work(session: ClientSession):
 
 # little message that the bot was started
 async def startup():
-    await client.send_message(LOG, "**[INFO]**\n\nUserbot was successfully started.")
+    await client.send_message(
+        constants.LOG, "**[INFO]**\n\nUserbot was successfully started."
+    )
 
 
 # shutdown handler in case the bot goes nuts (again)
-@client.on(events.NewMessage(outgoing=True, pattern=SHUTDOWN_COMMAND))
+@client.on(events.NewMessage(outgoing=True, pattern=constants.SHUTDOWN_COMMAND))
 async def shutdown_handler(_):
     logger.error("Initiating shut down")
-    await client.send_message(LOG, "**[INFO]**\n\nShutdown was successfully initiated.")
+    await client.send_message(
+        constants.LOG, "**[INFO]**\n\nShutdown was successfully initiated."
+    )
     # just so everything is saved - it should be anyway, but just to be sure
     database.save()
     # this stops the whole loop
@@ -358,5 +395,9 @@ async def main():
 
 
 if __name__ == "__main__":
+    if not os.path.isfile("./database.json"):
+        asyncio.run(setup(intro_printed))
+        # Reinitialize because json is only now generated
+        database = Database()
     asyncio.run(main())
     logger.error("Shut down complete")
