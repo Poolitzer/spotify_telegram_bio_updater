3db9aca13905979086e054022ee9a2e8e73f4b96
Tomi Prasetio <tomy.prs25@gmail.com>
2022-02-01 21:38:30 +0700

    generate: Make run the async function on a reactor
    It's fixed my second issue https://github.com/Poolitzer/spotify_telegram_bio_updater/issues/5#issuecomment-1026063221

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from constants import CLIENT_SECRET, CLIENT_ID, INITIAL_BIO, INITIAL_TOKEN
 
@@ -11,17 +12,27 @@ body = {
     "code": INITIAL_TOKEN,
 }
 
-async with ClientSession() as session:
-    async with session.post(
-        "https://accounts.spotify.com/api/token", data=body
-    ) as post_response:
-        save = await post_response.json()
-        to_create = {
-            "bio": INITIAL_BIO,
-            "access_token": save["access_token"],
-            "refresh_token": save["refresh_token"],
-            "telegram_spam": False,
-            "spotify_spam": False,
-        }
-        with open("./database.json", "w") as outfile:
-            json.dump(to_create, outfile, indent=4, sort_keys=True)
+async def generate():
+    async with ClientSession() as session:
+        async with session.post(
+            "https://accounts.spotify.com/api/token", data=body
+        ) as post_response:
+            save = await post_response.json()
+            to_create = {
+                "bio": INITIAL_BIO,
+                "access_token": save["access_token"],
+                "refresh_token": save["refresh_token"],
+                "telegram_spam": False,
+                "spotify_spam": False,
+            }
+            with open("./database.json", "w") as outfile:
+                json.dump(to_create, outfile, indent=4, sort_keys=True)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(generate())
+    loop.close()
+
+if __name__ == '__main__':
+    main()
